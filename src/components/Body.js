ad5df1a34913a2123f661fa603e420e50cdb7dc1
Shiv Kumar <shiv.gugansingh@sifycorp.com>
2023-03-06 13:18:36 +0530

    Issue: search button works

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import { restaurnatList } from "../constants";
 import ReasturantCard from "./RestaurantCard";
+import { useState } from "react";
+
+function filterData(searchInput) {
+  console.log(restaurnatList.length);
+  if (searchInput.length > 0) {
+    console.log("no");
+    return restaurnatList.filter((restaurant) =>
+      restaurant?.data?.name.toLocaleLowerCase().includes(searchInput)
+    );
+  } else {
+    console.log("yes");
+    return restaurnatList;
+  }
+}
 
 const Body = () => {
+  // const searchTxt = "KFC";
+
+  // searchText is a local state variable
+  const [restaurants, setRestaurants] = useState(restaurnatList);
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <>
       <div className="search-restro">
@@ -9,12 +30,23 @@ const Body = () => {
           type="text"
           className="search-input"
           placeholder="Search for restaurant and food"
-          value=""
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
         />
-        <button className="btn-search">Search</button>
+        <button
+          className="btn-search"
+          onClick={() => {
+            const data = filterData(searchInput);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="restaurnat-list">
-        {restaurnatList.map((restaurant) => {
+        {restaurants.map((restaurant) => {
           return (
             <ReasturantCard {...restaurant.data} key={restaurant.data.id} />
           );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-const Title = () => (
+export const Title = () => (
   <img src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/blackoption_200x.png?v=1659016547" />
 );
 const HeaderComponent = () => (
   <div className="header">
     <nav>
-      <Title></Title>
+      <Title />
       <div>
         <ul className="nav-links">
           <li>Home</li>
