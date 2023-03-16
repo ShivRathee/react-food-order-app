8444ce361c123ced68dc9370bc351bc873a5f27a
Shiv Kumar <shiv.gugansingh@sifycorp.com>
2023-03-16 18:10:05 +0530

    Issue: using useEffect to call swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { restaurnatList } from "../constants";
 import ReasturantCard from "./RestaurantCard";
-import { useState } from "react";
 
-function filterData(searchInput) {
-  console.log(restaurnatList.length);
-  if (searchInput.length > 0) {
-    console.log("no");
-    return restaurnatList.filter((restaurant) =>
-      restaurant?.data?.name.toLocaleLowerCase().includes(searchInput)
-    );
-  } else {
-    console.log("yes");
-    return restaurnatList;
-  }
+function filterData(searchInput, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchInput)
+  );
 }
 
 const Body = () => {
   // const searchTxt = "KFC";
 
   // searchText is a local state variable
-  const [restaurants, setRestaurants] = useState(restaurnatList);
+  const [restaurants, setRestaurants] = useState([]);
   const [searchInput, setSearchInput] = useState("");
+
+  // empty dependency array => once after render
+  // dep array[searchInput] => once after initial render + everytime after render (my searchInput changes)
+  useEffect(() => {
+    // API call
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // optional chaining
+    setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
 
   return (
     <>
@@ -38,7 +47,7 @@ const Body = () => {
         <button
           className="btn-search"
           onClick={() => {
-            const data = filterData(searchInput);
+            const data = filterData(searchInput, restaurants);
             setRestaurants(data);
           }}
         >
